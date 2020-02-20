f64f30dc97828abcd7888afea9dd40c4536c4b96
ccpic <13426257846@163.com>
2020-02-20 10:31:42 +0800

    Add search function in ClientAdmin

--- SalesExpense/clientfile/admin.py
+++ SalesExpense/clientfile/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from .models import Client
 
+
+class ClientAdmin(admin.ModelAdmin):
+    search_fields = ['rd', 'rm', 'dsm', 'rsp', 'hospital',  'dept', 'name']
+
 # Register your models here.
-admin.site.register([Client])
+admin.site.register(Client, ClientAdmin)
+
